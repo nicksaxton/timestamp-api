40a791c9d2ca3d3468f203b3ad95bb163eac40b5
Nick <nick.saxton@gmail.com>
2017-01-03 21:21:32 +0000

    Working with both integer input and natural language

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+app.get('/:time', function (req, res) {
+    var unixPatt = /^[0-9]+$/;
+    
+    var result = {
+        "unix": null,
+        "natural": null
+    };
+    
+    if (unixPatt.test(req.params.time)) {
+        var d = new Date(parseInt(req.params.time));
+        result.unix = d.getTime();
+        result.natural = months[d.getMonth()] + " " + d.getUTCDate() + ", " + d.getUTCFullYear();
+    }
+    else {
+        var d = new Date(req.params.time);
+       
+        result.unix = d.getTime();
+        
+        if (d.getTime()) {
+            result.natural = months[d.getMonth()] + " " + d.getUTCDate() + ", " + d.getUTCFullYear();
+        }
+    }
+    
+    res.send(JSON.stringify(result));
 })
 
 app.listen(8080, function () {
